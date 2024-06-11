9d6633f070859e93c16bc284bc89ac59688e88af
Cauã <92538361+raulino20@users.noreply.github.com>
2024-06-11 16:32:37 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,93 @@
+let Arr = []
+
+let juncao = document.getElementById('Iterar')
+const Confirm = document.getElementById('Confirmar')
+
+
+Confirm.addEventListener('click', function(){
+
+let Modelo = document.getElementById('Car').value
+let Placa = document.getElementById('CarNum').value
+let Estacionamento = document.getElementById('Resultado')
+
+
+
+const Veiculo = {
+
+    Carro : Modelo,
+    Numero : Placa
+
+  }
+
+
+if(Modelo && Placa ){
+
+Arr.push(Veiculo)
+
+
+Adicionar = ""
+for(let i = 0; i < Arr.length; i++ ){
+
+    Adicionar += (i + 1) + "- " + "Associado: " + Arr[i].Carro +  " de Numero : " + Arr[i].Numero + "\n" 
+
+}
+console.log(Adicionar)
+console.log(Arr)
+
+Iterar.innerText = 'Contingente : ' + Arr.length
+Estacionamento.innerText = Adicionar
+
+ document.getElementById('Car').value = ""
+ document.getElementById('CarNum').value = ""
+
+}else{
+    alert("Um ou mais campos não foram prenchidos !")
+    console.error('ERROR #27072001')
+    document.getElementById('Car').value = ""
+    document.getElementById('CarNum').value = ""
+}
+})
+
+const Remove = document.getElementById('Remove')
+
+Remove.addEventListener('click', function(){
+
+let Estacionamento = document.getElementById('Resultado')
+ const Veicular = document.getElementById('Carro').value
+ let juncao = document.getElementById('Iterar')
+
+let Search = Arr.findIndex(Busca => Busca.Carro === Veicular || Busca.Numero === Veicular )
+
+
+    if(Search !== -1){
+
+        const Confirmacao = confirm('Deseja mesmo remover o veiculo ? ')
+
+    if(Confirmacao === true){
+
+        Arr.splice(Search,1)
+
+        let Adicionar = ""
+
+    for(let i = 0; i < Arr.length; i++){
+ 
+        Adicionar += (i + 1) +("- ") + "Associado : " + Arr[i].Carro + " de Numero : " + Arr[i].Numero + "\n"
+ 
+    }
+    Estacionamento.innerText = Adicionar
+    juncao.innerText = 'Contingente : ' + Arr.length
+    document.getElementById('Carro').value = ""
+    console.log(Arr)
+   }else{
+
+    alert("O VEICULO CONTINUARÁ ESTACIONADO ! ")
+   
+}
+  }else{
+
+    alert("VEOCULO NÃO ENCONTRADO ! ")
+    console.warn('VEICULONAOENCONTRADO #20702001');
+
+  }
+})
+
