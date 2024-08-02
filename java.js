92c07b88b05b804eed491e3f2947c957dbcd7899
Cauã <92538361+raulino20@users.noreply.github.com>
2024-08-02 15:08:30 -0300

    Update java.js

--- java.js
+++ java.js
@@ -164,7 +164,7 @@ let Search = Arr.findIndex(Busca => Busca.Nome.toLowerCase() === Veicular.trim()
 
      alert("PARTICIPANTE NÃO ENCONTRADO ! ")
      console.warn('PARTICIPANTENAOENCONTRADO #20702001');
-     document.getElementById('Nome').value = ""
+     document.getElementById('NomeX').value = ""
      console.log(Arr)
      Estacionamento.innerText = Adicionar
   }
